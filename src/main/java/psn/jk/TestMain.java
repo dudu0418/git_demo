bbc6bf24244633ec2534ea49cdbfd90eb760bd57
dudu0418 <sjinqun@163.com>
2014-11-20 00:02:11 +0800

    首次提交本地仓库

---
+++ src/main/java/psn/jk/TestMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package psn.jk;
+
+/**
+ * @author Administrator
+ *
+ */
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println( "Test Main" );
+	}
+
+}
